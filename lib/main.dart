b636d4bafab033b6b2df6000559a60ac72594a03
thienot <thienot280904@gmail.com>
2024-02-18 22:18:37 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: SafeArea(
+      child: DemoApp(),
+    ),
+  ));
+}
+
+class DemoApp extends StatefulWidget {
+  const DemoApp({super.key});
+
+  @override
+  State<DemoApp> createState() => _DemoAppState();
+}
+
+class _DemoAppState extends State<DemoApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          ElevatedButton(
+              onPressed: (){},
+              child: Text('Search'),
+          )
+        ],
+      ),
+
+
+    );
+  }
+}
+
